fab8b3bd1d0dae482be4a25b707337e3db502445
Nikao777 <139736216+Nicolas7720@users.noreply.github.com>
2025-01-28 05:03:48 -0300

    Mudei o código
    Peguei a lista toda com o que selectorAll w colooquei em uma variavel, usando o nome da variavel usei o forEach e chamei o elemento de tecla, para  cada item adicionei o evento de click, nele eu criei uma var para pegar o texto da tecla e fiz eles ficarem em minusculo e sem espaço, com o query selector para id, eu repeti o id  e adicionei a var com o nome da tecla pois cada id é igual e só muda o final que é nome da tecla em minusculo, por fim eu usei o play() para tocar

--- aluramidi-curso-arquivos-iniciais/script/script.js
+++ aluramidi-curso-arquivos-iniciais/script/script.js
@@ -1,20 +1,8 @@
-const sons = [
-    { nome: 'tecla_pom', som: 'som_tecla_pom' },
-    { nome: 'tecla_clap', som: 'som_tecla_clap' },
-    { nome: 'tecla_tim', som: 'som_tecla_tim' },
-    { nome: 'tecla_puff', som: 'som_tecla_puff' },
-    { nome: 'tecla_splash', som: 'som_tecla_splash' },
-    { nome: 'tecla_toim', som: 'som_tecla_toim' },
-    { nome: 'tecla_psh', som: 'som_tecla_psh' },
-    { nome: 'tecla_tic', som: 'som_tecla_tic' },
-    { nome: 'tecla_tom', som: 'som_tecla_tom' },
-]
+const listaDeTeclas = document.querySelectorAll(".tecla")
 
-
-sons.map((som) => (
-    document.querySelector(`.${som.nome}`).addEventListener('click', () => {
-        const elementoMusical = document.querySelector(`#${som.som}`)
-        elementoMusical.play()
-
+listaDeTeclas.forEach((tecla) => {
+    tecla.addEventListener("click", () => {
+        const nomeTecla = tecla.textContent.trim().toLowerCase()
+        document.querySelector(`#som_tecla_${nomeTecla}`).play()
     })
-))
+})
