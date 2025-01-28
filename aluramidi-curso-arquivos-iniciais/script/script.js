7417fbdb8f5b368de9451def0b88ac9a25e732f9
Nikao777 <139736216+Nicolas7720@users.noreply.github.com>
2025-01-28 05:14:16 -0300

    codigo antigo comentado

--- aluramidi-curso-arquivos-iniciais/script/script.js
+++ aluramidi-curso-arquivos-iniciais/script/script.js
@@ -1,3 +1,24 @@
+// const sons = [
+//     { nome: 'pom' },
+//     { nome: 'clap' },
+//     { nome: 'tim' },
+//     { nome: 'puff' },
+//     { nome: 'splash' },
+//     { nome: 'toim' },
+//     { nome: 'psh' },
+//     { nome: 'tic' },
+//     { nome: 'tom' },
+// ]
+
+
+// sons.map((som) => (
+//     document.querySelector(`.tecla_${som.nome}`).addEventListener('click', () => {
+//         const elementoMusical = document.querySelector(`#som_tecla_${som.nome}`)
+//         elementoMusical.play()
+
+//     })
+// ))
+
 const listaDeTeclas = document.querySelectorAll(".tecla")
 
 listaDeTeclas.forEach((tecla) => {
