a2eb69835098e883a0d3998fb16c66858ff465ef
Nikao777 <139736216+Nicolas7720@users.noreply.github.com>
2025-01-28 06:23:51 -0300

    projeto finalizado

--- aluramidi-curso-arquivos-iniciais/script/script.js
+++ aluramidi-curso-arquivos-iniciais/script/script.js
@@ -26,10 +26,10 @@ listaDeTeclas.forEach((tecla) => {
         const nomeTecla = tecla.textContent.trim().toLowerCase()
         const elemento = document.querySelector(`#som_tecla_${nomeTecla}`)
 
-        if (elemento === null) {
-            console.log('Elemento não encontrado')
-        } else {
+        if (elemento && elemento.localName === 'audio') {
             elemento.play()
+        } else {
+            console.log('Elemento não encontrado ou seletor inválido')
         }
 
         tecla.addEventListener("keydown", (e) => {
